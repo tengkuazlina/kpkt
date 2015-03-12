ece9fd7c1fac479d888f4649751305e7d81dcb12
Tengku Azlina <tengkuazlina@users.noreply.github.com>
2015-03-12 16:07:49 +0800

    Create cuba.php
    testing mengantuk

---
+++ cuba.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello World";
+
+echo "This spans
+multiple lines. The newlines will be
+output as well";
+
+echo "This spans\nmultiple lines. The newlines will be\noutput as well.";
+
+echo "Escaping characters is done \"Like this\".";
+?>php
